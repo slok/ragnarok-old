ae4372a48321c36706db6ca726684b88d04bade1
Xabier Larrakoetxea <slok69@gmail.com>
2017-04-22 17:23:51 +0200

    Add failure doc file
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

---
+++ failure/doc.go
@@ -0,0 +1,20 @@
+/*
+Package failure manages all the failure types. A failure among other properties it is made
+from multiple attacks. these attacks will be started for a failure defined duration, after
+this defined period the failure will be reverted.
+
+We hae to take into account that there are different kind of attacks, on kind of attack is
+a permanen attack based on duration, for example a memory allocation: 'allocate 100MiB of
+memory for 60m then free these 100MiB of memory'. On the other hand we have attacks that are
+periodic but adding a new failure on top of previous one , for example a memory leak, a memory
+leak is a periodic memory allocation attack: allocate 10MiB every 30s for 60m. A
+third kind of attack can be a periodic attack not based on the previous state, for example a
+CPU spike every of 1m every 20m for 4h.
+
+there are a lot of attacks with different nature and a failure can have multiple attacks at
+the same time, taking this into account this will the responsability of the attack itself
+(periods, flushing run/state, adding on top of previous run/state of the periodic loop, etc)
+
+configuraiton/defition examples can be check on failure/testdata path
+*/
+package failure // import "github.com/slok/ragnarok/failure"
