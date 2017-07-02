8d96c604458b427520e5518588673b11637a59c1
Xabier Larrakoetxea <slok69@gmail.com>
2017-07-02 11:05:34 +0200

    Add simple node main
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

---
+++ cmd/node/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/slok/ragnarok/cmd/node/flags"
+	"github.com/slok/ragnarok/log"
+	"github.com/slok/ragnarok/node"
+)
+
+// Main run main logic
+func Main() error {
+	logger := log.Base()
+
+	// Get the command line arguments
+	cfg, err := flags.GetNodeConfig(os.Args[1:])
+	if err != nil {
+		logger.Error(err)
+		return err
+	}
+
+	// Set debug mode
+	if cfg.Debug {
+		logger.Set("debug")
+	}
+
+	// Create the node
+	n := node.NewFailureNode(*cfg, logger)
+	n.GetID()
+
+	return nil
+}
+
+func clean() {
+	log.Debug("Cleaning...")
+}
+
+func main() {
+	sigC := make(chan os.Signal, 1)
+	signal.Notify(sigC, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	errC := make(chan error)
+
+	// Run main program
+	go func() {
+		if err := Main(); err != nil {
+			errC <- err
+		}
+		return
+	}()
+
+	// Wait until signal (ctr+c, SIGTERM...)
+	var exitCode int
+
+Waiter:
+	for {
+		select {
+		// Wait for errors
+		case err := <-errC:
+			if err != nil {
+				exitCode = 1
+				break Waiter
+			}
+			// Wait for signal
+		case <-sigC:
+			break Waiter
+		}
+	}
+
+	clean()
+	os.Exit(exitCode)
+}
