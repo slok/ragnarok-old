a2c594451346724f8da40c5763f10cfab72f9d97
Xabier Larrakoetxea <slok69@gmail.com>
2017-07-05 18:19:59 +0200

    First client register node by grpc implementation
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

--- node/client/status.go
+++ node/client/status.go
@@ -1,8 +1,67 @@
 package client
 
+import (
+	"context"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/slok/ragnarok/grpc/nodestatus"
+	"github.com/slok/ragnarok/log"
+)
+
 // Status interface will implement the required methods to be able to communicate
 // with a node status server
 type Status interface {
 	// RegisterNode registers a node as available on the server
 	RegisterNode(id string, tags map[string]string) error
+}
+
+// StatusGRPC satisfies Status interface with GRPC communication
+type StatusGRPC struct {
+	c      pb.NodeStatusClient
+	logger log.Logger
+}
+
+// NewStatusGRPCFromConnection returns a new Status GRPC client based on the grpc connection
+func NewStatusGRPCFromConnection(connection *grpc.ClientConn, logger log.Logger) (*StatusGRPC, error) {
+	c := pb.NewNodeStatusClient(connection)
+	return NewStatusGRPC(c, logger)
+}
+
+// NewStatusGRPC returns a new Status GRPC client
+func NewStatusGRPC(client pb.NodeStatusClient, logger log.Logger) (*StatusGRPC, error) {
+	logger = logger.WithField("service", "node-status").WithField("service-kind", "grpc")
+	return &StatusGRPC{
+		c:      client,
+		logger: logger,
+	}, nil
+}
+
+// RegisterNode satisfies Status interface
+func (s *StatusGRPC) RegisterNode(id string, tags map[string]string) error {
+	logger := s.logger.WithField("call", "register-node").WithField("id", id)
+	logger.Debug("making GRPC service call")
+
+	// Create the request objects
+	ni := &pb.NodeInfo{
+		Node: &pb.Node{
+			Id: id,
+		},
+		Address: "0.0.0.0", // TODO: Set up correct address
+		Tags:    tags,
+	}
+
+	// Make the request synchronously
+	resp, err := s.c.Register(context.Background(), ni)
+
+	// Call error
+	if err != nil {
+		return err
+	}
+	// If we have a response then (we should, just checking)
+	if resp != nil {
+		logger.Debug("call response: %s", resp.Message)
+	}
+
+	return nil
 }
---
+++ node/client/status_test.go
@@ -0,0 +1,63 @@
+package client_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+
+	pb "github.com/slok/ragnarok/grpc/nodestatus"
+	"github.com/slok/ragnarok/log"
+	mgrpc "github.com/slok/ragnarok/mocks/grpc"
+	"github.com/slok/ragnarok/node/client"
+)
+
+func TestRegisterNode(t *testing.T) {
+	assert := assert.New(t)
+	tests := []struct {
+		id          string
+		tags        map[string]string
+		expectError bool
+	}{
+		{"test1", map[string]string{}, false},
+		{"test2", map[string]string{}, true},
+		{"test2", map[string]string{"key1": "value1"}, false},
+		{"test2", map[string]string{"key1": "value1", "key2": "value2"}, true},
+	}
+
+	for _, test := range tests {
+
+		// Mocks
+		mc := &mgrpc.NodeStatusClient{}
+		var expectErr error
+		if test.expectError {
+			expectErr = errors.New("wanted error")
+		}
+
+		expectNI := &pb.NodeInfo{
+			Node: &pb.Node{
+				Id: test.id,
+			},
+			Address: "0.0.0.0",
+			Tags:    test.tags,
+		}
+		resp := &pb.RegisteredResponse{Message: "called"}
+		mc.On("Register", mock.Anything, expectNI).Once().Return(resp, expectErr)
+
+		// Create our client and make our service call
+		s, err := client.NewStatusGRPC(mc, log.Dummy)
+		if assert.NoError(err) {
+			// Check check result is ok
+			err := s.RegisterNode(test.id, test.tags)
+			if test.expectError {
+				assert.Error(err)
+			} else {
+				assert.NoError(err)
+			}
+
+		}
+		// Check calls where good
+		mc.AssertExpectations(t)
+	}
+}
