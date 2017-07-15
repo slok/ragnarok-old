f20972873ee1210b0935fc2172845a18385035e8
Xabier Larrakoetxea <slok69@gmail.com>
2017-07-15 09:37:45 +0200

    Add nodeheartbeat to the node
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

--- node/client/status.go
+++ node/client/status.go
@@ -5,8 +5,9 @@ import (
 
 	"google.golang.org/grpc"
 
-	pb "github.com/slok/ragnarok/grpc/nodestatus"
+	pbns "github.com/slok/ragnarok/grpc/nodestatus"
 	"github.com/slok/ragnarok/log"
+	"github.com/slok/ragnarok/types"
 )
 
 // Status interface will implement the required methods to be able to communicate
@@ -14,26 +15,30 @@ import (
 type Status interface {
 	// RegisterNode registers a node as available on the server
 	RegisterNode(id string, tags map[string]string) error
+	// NodeHeartbeat sends a node heartbeat to the master
+	NodeHeartbeat(id string, status types.NodeState)
 }
 
 // StatusGRPC satisfies Status interface with GRPC communication
 type StatusGRPC struct {
-	c      pb.NodeStatusClient
-	logger log.Logger
+	c           pbns.NodeStatusClient
+	stateParser types.NodeStateParser
+	logger      log.Logger
 }
 
 // NewStatusGRPCFromConnection returns a new Status GRPC client based on the grpc connection
-func NewStatusGRPCFromConnection(connection *grpc.ClientConn, logger log.Logger) (*StatusGRPC, error) {
-	c := pb.NewNodeStatusClient(connection)
-	return NewStatusGRPC(c, logger)
+func NewStatusGRPCFromConnection(connection *grpc.ClientConn, stateParser types.NodeStateParser, logger log.Logger) (*StatusGRPC, error) {
+	c := pbns.NewNodeStatusClient(connection)
+	return NewStatusGRPC(c, stateParser, logger)
 }
 
 // NewStatusGRPC returns a new Status GRPC client
-func NewStatusGRPC(client pb.NodeStatusClient, logger log.Logger) (*StatusGRPC, error) {
+func NewStatusGRPC(client pbns.NodeStatusClient, stateParser types.NodeStateParser, logger log.Logger) (*StatusGRPC, error) {
 	logger = logger.WithField("service", "node-status").WithField("service-kind", "grpc")
 	return &StatusGRPC{
-		c:      client,
-		logger: logger,
+		c:           client,
+		stateParser: stateParser,
+		logger:      logger,
 	}, nil
 }
 
@@ -43,7 +48,7 @@ func (s *StatusGRPC) RegisterNode(id string, tags map[string]string) error {
 	logger.Debug("making GRPC service call")
 
 	// Create the request objects
-	n := &pb.Node{
+	n := &pbns.Node{
 		Id:   id,
 		Tags: tags,
 	}
@@ -55,10 +60,32 @@ func (s *StatusGRPC) RegisterNode(id string, tags map[string]string) error {
 	if err != nil {
 		return err
 	}
-	// If we have a response then (we should, just checking)
+	// If we have a response then (we should)
 	if resp != nil {
 		logger.Debugf("call response: %s", resp.Message)
 	}
 
+	return nil
+}
+
+// NodeHeartbeat satisfies Status interface
+func (s *StatusGRPC) NodeHeartbeat(id string, state types.NodeState) error {
+	logger := s.logger.WithField("call", "node-heartbeat").WithField("id", id)
+	logger.Debug("making GRPC service call")
+
+	st, err := s.stateParser.NodeStateToPB(state)
+	if err != nil {
+		return err
+	}
+
+	ns := &pbns.NodeState{
+		Id:    id,
+		State: st,
+	}
+
+	if _, err := s.c.Heartbeat(context.Background(), ns); err != nil {
+		return err
+	}
+	logger.Debugf("heartbeat succeeded")
 	return nil
 }
--- node/client/status_test.go
+++ node/client/status_test.go
@@ -7,10 +7,12 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 
-	pb "github.com/slok/ragnarok/grpc/nodestatus"
+	pbns "github.com/slok/ragnarok/grpc/nodestatus"
 	"github.com/slok/ragnarok/log"
 	mgrpc "github.com/slok/ragnarok/mocks/grpc"
+	mtypes "github.com/slok/ragnarok/mocks/types"
 	"github.com/slok/ragnarok/node/client"
+	"github.com/slok/ragnarok/types"
 )
 
 func TestRegisterNode(t *testing.T) {
@@ -22,8 +24,8 @@ func TestRegisterNode(t *testing.T) {
 	}{
 		{"test1", map[string]string{}, false},
 		{"test2", map[string]string{}, true},
-		{"test2", map[string]string{"key1": "value1"}, false},
-		{"test2", map[string]string{"key1": "value1", "key2": "value2"}, true},
+		{"test3", map[string]string{"key1": "value1"}, false},
+		{"test4", map[string]string{"key1": "value1", "key2": "value2"}, true},
 	}
 
 	for _, test := range tests {
@@ -35,15 +37,15 @@ func TestRegisterNode(t *testing.T) {
 			expectErr = errors.New("wanted error")
 		}
 
-		expectN := &pb.Node{
+		expectN := &pbns.Node{
 			Id:   test.id,
 			Tags: test.tags,
 		}
-		resp := &pb.RegisteredResponse{Message: "called"}
+		resp := &pbns.RegisteredResponse{Message: "called"}
 		mc.On("Register", mock.Anything, expectN).Once().Return(resp, expectErr)
 
 		// Create our client and make our service call
-		s, err := client.NewStatusGRPC(mc, log.Dummy)
+		s, err := client.NewStatusGRPC(mc, types.NodeStateTransformer, log.Dummy)
 		if assert.NoError(err) {
 			// Check check result is ok
 			err := s.RegisterNode(test.id, test.tags)
@@ -56,5 +58,62 @@ func TestRegisterNode(t *testing.T) {
 		}
 		// Check calls where good
 		mc.AssertExpectations(t)
+	}
+}
+
+func TestNodeHeartbeat(t *testing.T) {
+	assert := assert.New(t)
+
+	tests := []struct {
+		id              string
+		state           types.NodeState
+		expState        pbns.State
+		expStParseError bool
+		expRespError    bool
+	}{
+		{"test1", types.ReadyNodeState, pbns.State_READY, false, false},
+		{"test2", types.AttackingNodeState, pbns.State_ATTACKING, false, false},
+		{"test3", types.ReadyNodeState, pbns.State_READY, true, false},
+		{"test4", types.ReadyNodeState, pbns.State_READY, false, true},
+	}
+
+	for _, test := range tests {
+		var expStParseErr error
+		var expRespErr error
+
+		if test.expStParseError {
+			expStParseErr = errors.New("wanted error")
+		}
+
+		if test.expRespError {
+			expRespErr = errors.New("wanted error")
+		}
+
+		// Create the mocks.
+		mc := &mgrpc.NodeStatusClient{}
+		mstp := &mtypes.NodeStateParser{}
+		ns := &pbns.NodeState{
+			Id:    test.id,
+			State: test.expState,
+		}
+		mstp.On("NodeStateToPB", mock.Anything).Once().Return(test.expState, expStParseErr)
+		// Don't call the client if there is a previous error.
+		if !test.expStParseError {
+			mc.On("Heartbeat", mock.Anything, ns).Once().Return(nil, expRespErr)
+		}
+
+		// Create the client.
+		s, err := client.NewStatusGRPC(mc, mstp, log.Dummy)
+		if assert.NoError(err) {
+			err := s.NodeHeartbeat(test.id, test.state)
+			if test.expRespError || test.expStParseError {
+				assert.Error(err)
+			} else {
+				assert.NoError(err)
+			}
+
+			mc.AssertExpectations(t)
+			mstp.AssertExpectations(t)
+		}
 	}
 }
