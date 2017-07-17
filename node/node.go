09787fcc59cde05ca530711e95f69fdd6a32a5ed
Xabier Larrakoetxea <slok69@gmail.com>
2017-07-17 19:30:30 +0200

    Add heartbeat on node
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

--- node/node.go
+++ node/node.go
@@ -1,47 +1,74 @@
 package node
 
 import (
+	"fmt"
+	"sync"
+	"time"
+
 	"github.com/google/uuid"
 
+	"github.com/slok/ragnarok/clock"
 	"github.com/slok/ragnarok/log"
 	"github.com/slok/ragnarok/node/client"
 	"github.com/slok/ragnarok/node/config"
+	"github.com/slok/ragnarok/types"
 )
 
-// Node is the interface that a node needs to implement to be a failure node
+// Node is the interface that a node needs to implement to be a failure node.
 type Node interface {
-	// RegisterOnMaster registers the node on the master
+	// RegisterOnMaster registers the node on the master.
 	RegisterOnMaster() error
-	// DeregisterOnMaster deregisters the node on the master
+	// DeregisterOnMaster deregisters the node on the master.
 	DeregisterOnMaster() error
-	// Serve serves the RPC and HTTP services
+	// Serve serves the RPC and HTTP services.
 	Serve() error
-	// GetID Gets the unique ID of the node
+	// GetID Gets the unique ID of the node.
 	GetID() string
+	// StartHeartbeat starts a heartbeat interval to the master.
+	StartHeartbeat() error
+	// StopHeartbeat stops a heartbeat interval.
+	StopHeartbeat() error
 }
 
-// FailureNode is a kind of node that injects failure on the host
+// FailureNode is a kind of node that injects failure on the host.
 type FailureNode struct {
-	id string
-
+	id           string
+	cfg          config.Config
+	clock        clock.Clock
 	log          log.Logger
 	dryRun       bool
 	debug        bool
-	statusClient client.Status // client to communicate with node status service
+	statusClient client.Status   // Client to communicate with node status service.
+	state        types.NodeState // The status of the node.
+
+	// Heartbeat
+	hbT       *time.Ticker  // Ticker of the heartbeat.
+	hbFinishC chan struct{} // Used to notify that the heartbeat has finished
+
+	// Mutexes
+	stMu  sync.Mutex // Mutex for the state.
+	hbTMu sync.Mutex // Mutex for the heartbeat ticker.
 }
 
-// NewFailureNode returns a new FailureNode instnace
-func NewFailureNode(cfg config.Config, statusClient client.Status, logger log.Logger) *FailureNode {
+// NewFailureNode returns a new FailureNode instance.
+func NewFailureNode(cfg config.Config,
+	statusClient client.Status,
+	clock clock.Clock,
+	logger log.Logger) *FailureNode {
+
 	id := uuid.New().String()
 
 	logger = logger.WithField("id", id)
 
 	f := &FailureNode{
 		id:           id,
+		cfg:          cfg,
+		clock:        clock,
 		log:          logger,
 		dryRun:       cfg.DryRun,
 		debug:        cfg.Debug,
 		statusClient: statusClient,
+		state:        types.UnknownNodeState,
 	}
 
 	logger.Info("System failure node ready")
@@ -53,12 +80,76 @@ func NewFailureNode(cfg config.Config, statusClient client.Status, logger log.Lo
 	return f
 }
 
-// GetID satisfies FailureNode interface
+// GetID satisfies FailureNode interface.
 func (f *FailureNode) GetID() string {
 	return f.id
 }
 
-// RegisterOnMaster satisfies FailureNode interface
+// RegisterOnMaster satisfies FailureNode interface.
 func (f *FailureNode) RegisterOnMaster() error {
+	// TODO: set the node state to ready state
 	return f.statusClient.RegisterNode(f.id, map[string]string{})
+}
+
+// StartHeartbeat satisfies FailureNode interface.
+func (f *FailureNode) StartHeartbeat() error {
+	f.hbTMu.Lock()
+	t := f.hbT
+	f.hbTMu.Unlock()
+
+	if t != nil {
+		return fmt.Errorf("seems that the heartbeat is already working")
+	}
+
+	// Create a new ticker in each heartbeat interval.
+	f.hbTMu.Lock()
+	t = f.clock.NewTicker(f.cfg.HeartbeatInterval)
+	f.hbT = t
+	f.hbTMu.Unlock()
+
+	// Set our heartbeat control channel.
+	f.hbFinishC = make(chan struct{})
+
+	// Start a heatbeat in a periodic interval.
+	f.log.Info("heartbeat started")
+	for {
+		select {
+		case <-f.hbFinishC:
+			f.log.Info("heartbeat stopped")
+			return nil
+
+		case <-t.C:
+			f.stMu.Lock()
+			st := f.state
+			f.stMu.Unlock()
+
+			// The show must go on.
+			if err := f.statusClient.NodeHeartbeat(f.id, st); err != nil {
+				f.log.Errorf("heartbeat failed: %v", err)
+			} else {
+				f.log.Debug("heartbeat sent")
+			}
+		}
+	}
+}
+
+// StopHeartbeat satisfies FailureNode interface.
+func (f *FailureNode) StopHeartbeat() error {
+	f.hbTMu.Lock()
+	defer f.hbTMu.Unlock()
+
+	// Check if theres is a heartbeat.
+	if f.hbT == nil {
+		return fmt.Errorf("heartbeat not running")
+	}
+
+	// Sent heartbeat stop signal.
+	close(f.hbFinishC)
+	f.hbFinishC = nil
+
+	// Stop heartbeat intervals for the GC.
+	f.hbT.Stop()
+	f.hbT = nil
+
+	return nil
 }
--- node/node_test.go
+++ node/node_test.go
@@ -3,22 +3,26 @@ package node_test
 import (
 	"errors"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 
+	"github.com/slok/ragnarok/clock"
 	"github.com/slok/ragnarok/log"
 	"github.com/slok/ragnarok/mocks"
+	mclient "github.com/slok/ragnarok/mocks/node/client"
 	"github.com/slok/ragnarok/node"
 	"github.com/slok/ragnarok/node/config"
+	"github.com/slok/ragnarok/types"
 )
 
 func TestFailureNodeCreation(t *testing.T) {
 	assert := assert.New(t)
 
-	scm := &mocks.Status{}
-	n := node.NewFailureNode(config.Config{}, scm, log.Dummy)
+	scm := &mclient.Status{}
+	n := node.NewFailureNode(config.Config{}, scm, clock.Base(), log.Dummy)
 	if assert.NotNil(n) {
 		assert.NotEmpty(n.GetID())
 	}
@@ -28,14 +32,14 @@ func TestFailureNodeCreationDryRun(t *testing.T) {
 	assert := assert.New(t)
 
 	// Mocks
-	scm := &mocks.Status{}
+	scm := &mclient.Status{}
 	logger := &mocks.Logger{}
 	logger.On("WithField", "id", mock.AnythingOfType("string")).Once().Return(logger)
 	logger.On("Info", "System failure node ready").Once()
 	logger.On("Warn", "System failure node in dry run mode").Once()
 
 	// Check
-	n := node.NewFailureNode(config.Config{DryRun: true}, scm, logger)
+	n := node.NewFailureNode(config.Config{DryRun: true}, scm, clock.Base(), logger)
 	if assert.NotNil(n) {
 		assert.NotEmpty(n.GetID())
 	}
@@ -46,10 +50,10 @@ func TestFailureNodeRegisterOnMasterOK(t *testing.T) {
 	require := require.New(t)
 
 	// Create the mock
-	scm := &mocks.Status{}
+	scm := &mclient.Status{}
 
-	// Create fNode and get the ID
-	n := node.NewFailureNode(config.Config{DryRun: true}, scm, log.Dummy)
+	// Create fNode and get the ID.
+	n := node.NewFailureNode(config.Config{DryRun: true}, scm, clock.Base(), log.Dummy)
 	require.NotNil(n)
 	id := n.GetID()
 
@@ -68,10 +72,11 @@ func TestFailureNodeRegisterOnMasterError(t *testing.T) {
 	require := require.New(t)
 
 	// Create the mock
-	scm := &mocks.Status{}
+	scm := &mclient.Status{}
 
-	// Create fNode and get the ID
-	n := node.NewFailureNode(config.Config{DryRun: true}, scm, log.Dummy)
+	// Create fNode and get the ID.
+	cfg := config.Config{DryRun: true}
+	n := node.NewFailureNode(cfg, scm, clock.Base(), log.Dummy)
 	require.NotNil(n)
 	id := n.GetID()
 
@@ -82,5 +87,154 @@ func TestFailureNodeRegisterOnMasterError(t *testing.T) {
 	err := n.RegisterOnMaster()
 	if assert.Error(err) {
 		scm.AssertExpectations(t)
+	}
+}
+
+func TestFailureNodeStartHeartbeatAlreadyRunningError(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	// Create the mock.
+	msc := &mclient.Status{}
+	mClock := &mocks.Clock{}
+	stubT := time.NewTicker(1 * time.Millisecond)
+	defer stubT.Stop()                   // Stop after test.
+	heartbeatting := make(chan struct{}) // Channel that will wait for the signal when the node is already sending heartbeats.
+	mClock.On("NewTicker", mock.AnythingOfType("time.Duration")).Once().Return(stubT).Run(func(args mock.Arguments) {
+		heartbeatting <- struct{}{}
+	})
+	msc.On("NodeHeartbeat", mock.Anything, mock.Anything).Return(nil)
+
+	// Create fNode and get the ID.
+	cfg := config.Config{}
+	n := node.NewFailureNode(cfg, msc, mClock, log.Dummy)
+	require.NotNil(n)
+
+	// Start first heartbeat in background.
+	go func() {
+		assert.NoError(n.StartHeartbeat())
+	}()
+
+	// Wait for the Goroutine started.
+	select {
+	case <-time.After(10 * time.Millisecond):
+		t.Fatal("timeout waiting for the heartbeat")
+		return
+	case <-heartbeatting:
+		// Run & Check.
+		err := n.StartHeartbeat()
+		assert.Error(err)
+		return
+	}
+}
+
+func TestFailureNodeStartHeartbeatOK(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	// Create the mock.
+	msc := &mclient.Status{}
+	mClock := &mocks.Clock{}
+	stubT := time.NewTicker(1 * time.Millisecond)
+	defer stubT.Stop()                    // Stop after test.
+	heartbeated := make(chan struct{}, 1) // Channel that will wait for the signal when the node heartbeated.
+	mClock.On("NewTicker", mock.AnythingOfType("time.Duration")).Once().Return(stubT)
+
+	// Create fNode.
+	cfg := config.Config{}
+	n := node.NewFailureNode(cfg, msc, mClock, log.Dummy)
+	require.NotNil(n)
+
+	// Mock heartbeat call.
+	msc.On("NodeHeartbeat", n.GetID(), types.UnknownNodeState).Return(nil).Run(func(args mock.Arguments) {
+		heartbeated <- struct{}{}
+	})
+
+	// Start first heartbeat in background.
+	go func() {
+		assert.NoError(n.StartHeartbeat())
+	}()
+
+	// Wait for the Goroutine started.
+
+	select {
+	case <-time.After(10 * time.Millisecond):
+		t.Fatal("timeout waiting for the heartbeat")
+		return
+	case <-heartbeated:
+		// Everything ok.
+		msc.AssertExpectations(t)
+		return
+	}
+}
+
+func TestFailureNodeStoptHeartbeatError(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	cfg := config.Config{}
+
+	// Create the mock.
+	msc := &mclient.Status{}
+
+	n := node.NewFailureNode(cfg, msc, clock.Base(), log.Dummy)
+	require.NotNil(n)
+
+	err := n.StopHeartbeat()
+	assert.Error(err)
+}
+
+func TestFailureNodeStoptHeartbeatOK(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	cfg := config.Config{}
+
+	// Create the mock.
+	msc := &mclient.Status{}
+	mClock := &mocks.Clock{}
+	heartbeated := make(chan struct{})       // Channel that will wait for the signal when the node heartbeated.
+	heartbeatFinished := make(chan struct{}) // Channel that will wait until heartbeat finishes.
+	stubT := time.NewTicker(1 * time.Millisecond)
+
+	// Mock calls.
+	msc.On("NodeHeartbeat", mock.Anything, types.UnknownNodeState).Return(nil).Run(func(args mock.Arguments) {
+		heartbeated <- struct{}{}
+	})
+	mClock.On("NewTicker", mock.AnythingOfType("time.Duration")).Once().Return(stubT)
+
+	n := node.NewFailureNode(cfg, msc, mClock, log.Dummy)
+	require.NotNil(n)
+
+	// Start heartbeat in background.
+	go func() {
+		assert.NoError(n.StartHeartbeat())
+		close(heartbeatFinished)
+	}()
+
+	// First wait unil start heartbeating.
+
+	select {
+	case <-time.After(10 * time.Millisecond):
+		t.Fatal("timeout waiting for the heartbeat")
+		return
+	case <-heartbeated:
+		// Everything ok.
+		msc.AssertExpectations(t)
+	}
+
+	// We are ready (already heartbeating9) to stop the heartbeating and check the stop.
+	err := n.StopHeartbeat()
+	require.NoError(err)
+
+	// Check that the heartbeat stopped.
+	select {
+	case <-time.After(10 * time.Millisecond):
+		t.Fatal("timeout waiting for the heartbeat to finish")
+		return
+	case <-heartbeatFinished:
+		// Everything ok.
+		msc.AssertExpectations(t)
+		return
 	}
 }
