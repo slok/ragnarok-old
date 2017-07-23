a7b3bb4cee0e4dd45f84b5fc73127cc15e4b360d
Xabier Larrakoetxea <slok69@gmail.com>
2017-07-23 14:52:56 +0200

    Add FailureState transformer
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

--- types/failurestate.go
+++ types/failurestate.go
@@ -1,5 +1,12 @@
 package types
 
+import (
+	"fmt"
+	"strings"
+
+	pbfs "github.com/slok/ragnarok/grpc/failurestatus"
+)
+
 // FailureState is the state a failure can be.
 type FailureState int
 
@@ -24,5 +31,63 @@ func (f FailureState) String() string {
 		return "disabled"
 	default:
 		return "unknown"
+	}
+}
+
+// FailureStateParser has the required methods to transform failure state.
+type FailureStateParser interface {
+	// StrToFailureState transforms an string to a failure state.
+	StrToFailureState(state string) (FailureState, error)
+	// PBToFailureState transforms a GRPC failure staet to a failure state.
+	PBToFailureState(state pbfs.State) (FailureState, error)
+	// FailureState transforms a failure state to a GRPC failure state
+	FailureStateToPB(state FailureState) (pbfs.State, error)
+}
+
+// failureStateParser will convert failure state in different types.
+type failureStateParser struct{}
+
+// FailureStateTransformer is the util to transform failure status kinds.
+var FailureStateTransformer = &failureStateParser{}
+
+// StrToFailureState implements FailureStateParser interface.
+func (f *failureStateParser) StrToFailureState(state string) (FailureState, error) {
+	switch strings.ToLower(state) {
+	case "enabled":
+		return EnabledFailureState, nil
+	case "reverting":
+		return RevertingFailureState, nil
+	case "disabled":
+		return DisabledFailureState, nil
+	default:
+		return UnknownFailureState, fmt.Errorf("invalid failure state: %s", state)
+	}
+}
+
+// PBToFailureState implements FailureStateParser interface.
+func (f *failureStateParser) PBToFailureState(state pbfs.State) (FailureState, error) {
+	switch state {
+	case pbfs.State_ENABLED:
+		return EnabledFailureState, nil
+	case pbfs.State_REVERTING:
+		return RevertingFailureState, nil
+	case pbfs.State_DISABLED:
+		return DisabledFailureState, nil
+	default:
+		return UnknownFailureState, fmt.Errorf("invalid failure state: %s", state)
+	}
+}
+
+// FailureStateToPB implements FailureStateParser interface.
+func (f *failureStateParser) FailureStateToPB(state FailureState) (pbfs.State, error) {
+	switch state {
+	case EnabledFailureState:
+		return pbfs.State_ENABLED, nil
+	case RevertingFailureState:
+		return pbfs.State_REVERTING, nil
+	case DisabledFailureState:
+		return pbfs.State_DISABLED, nil
+	default:
+		return pbfs.State_UNKNOWN, fmt.Errorf("invalid failure state: %s", state)
 	}
 }
--- types/failurestate_test.go
+++ types/failurestate_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
+	pbfs "github.com/slok/ragnarok/grpc/failurestatus"
 	"github.com/slok/ragnarok/types"
 )
 
@@ -24,5 +25,82 @@ func TestFailureStateStringer(t *testing.T) {
 
 	for _, test := range tests {
 		assert.Equal(test.expSt, test.st.String())
+	}
+}
+
+func TestFailureStateParseStrToFS(t *testing.T) {
+	assert := assert.New(t)
+
+	tests := []struct {
+		st     string
+		expSt  types.FailureState
+		expErr bool
+	}{
+		{"enabled", types.EnabledFailureState, false},
+		{"reverting", types.RevertingFailureState, false},
+		{"disabled", types.DisabledFailureState, false},
+		{"no-state", types.UnknownFailureState, true},
+	}
+
+	for _, test := range tests {
+		gotSt, err := types.FailureStateTransformer.StrToFailureState(test.st)
+		if test.expErr {
+			assert.Error(err)
+		} else {
+			assert.NoError(err)
+		}
+		assert.Equal(test.expSt, gotSt)
+	}
+}
+
+func TestFailureStateParsePBToFS(t *testing.T) {
+	assert := assert.New(t)
+
+	tests := []struct {
+		st     pbfs.State
+		expSt  types.FailureState
+		expErr bool
+	}{
+		{pbfs.State_ENABLED, types.EnabledFailureState, false},
+		{pbfs.State_REVERTING, types.RevertingFailureState, false},
+		{pbfs.State_DISABLED, types.DisabledFailureState, false},
+		{pbfs.State_UNKNOWN, types.UnknownFailureState, true},
+		{999999, types.UnknownFailureState, true},
+	}
+
+	for _, test := range tests {
+		gotSt, err := types.FailureStateTransformer.PBToFailureState(test.st)
+		if test.expErr {
+			assert.Error(err)
+		} else {
+			assert.NoError(err)
+		}
+		assert.Equal(test.expSt, gotSt)
+	}
+}
+
+func TestFailureStateParseFSToPB(t *testing.T) {
+	assert := assert.New(t)
+
+	tests := []struct {
+		st     types.FailureState
+		expSt  pbfs.State
+		expErr bool
+	}{
+		{types.EnabledFailureState, pbfs.State_ENABLED, false},
+		{types.RevertingFailureState, pbfs.State_REVERTING, false},
+		{types.DisabledFailureState, pbfs.State_DISABLED, false},
+		{types.UnknownFailureState, pbfs.State_UNKNOWN, true},
+		{999999, pbfs.State_UNKNOWN, true},
+	}
+
+	for _, test := range tests {
+		gotSt, err := types.FailureStateTransformer.FailureStateToPB(test.st)
+		if test.expErr {
+			assert.Error(err)
+		} else {
+			assert.NoError(err)
+		}
+		assert.Equal(test.expSt, gotSt)
 	}
 }
