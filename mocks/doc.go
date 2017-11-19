fe5dfb9848add1b6cacaafd6e2dc05efe04d4832
Xabier Larrakoetxea <slok69@gmail.com>
2017-11-19 16:18:00 +0100

    Update mocks
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

--- mocks/doc.go
+++ mocks/doc.go
@@ -25,7 +25,6 @@ package mocks // import "github.com/slok/ragnarok/mocks"
 // Services mocks
 //go:generate mockery -output ./master/service -outpkg service -dir ../master/service -name NodeStatusService
 //go:generate mockery -output ./master/service -outpkg service -dir ../master/service -name FailureStatusService
-//go:generate mockery -output ./master/service/repository -outpkg service -dir ../master/service/repository -name Failure
 //go:generate mockery -output ./master/service/scheduler -outpkg service -dir ../master/service/scheduler -name Scheduler
 
 // GRPC proto clients
@@ -39,7 +38,9 @@ package mocks // import "github.com/slok/ragnarok/mocks"
 //go:generate mockery -output ./apimachinery/validator -outpkg validator -dir ../apimachinery/validator -name ObjectValidator
 
 // client mocks
-//go:generate mockery -output ./client/cluster/v1 -outpkg v1 -dir ../client/cluster/v1 -name Node
+//go:generate mockery -output ./client/repository -outpkg v1 -dir ../client/repository -name Client
+//go:generate mockery -output ./client/api/cluster/v1 -outpkg v1 -dir ../client/api/cluster/v1 -name NodeClientInterface
+//go:generate mockery -output ./client/api/chaos/v1 -outpkg v1 -dir ../client/api/chaos/v1 -name FailureClientInterface
 
 // web mocks
 //go:generate mockery -output ./master/web/handler -outpkg serializer -dir ../master/web/handler -name Handler
