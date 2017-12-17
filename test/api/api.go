a5157f4022e524d9ebdf058134ba6a5ad87babe2
Xabier Larrakoetxea <slok69@gmail.com>
2017-12-17 18:42:32 +0100

    Add Object list type and NodeList resource
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

--- test/api/api.go
+++ test/api/api.go
@@ -16,7 +16,6 @@ func (t *TestObj) GetObjectVersion() api.Version { return t.Version }
 func (t *TestObj) GetObjectMetadata() api.ObjectMeta {
 	return api.ObjectMeta{ID: t.ID, Labels: t.Labels}
 }
-func (t *TestObj) GetListMetadata() api.ListMeta { return api.NoListMeta }
 func (t *TestObj) DeepCopy() api.Object {
 	copy := *t
 	return &copy
